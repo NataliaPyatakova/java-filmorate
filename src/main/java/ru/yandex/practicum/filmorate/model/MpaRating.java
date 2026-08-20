d524e0aaac8b9fce6483f4e568499773ae50c429
Natalia Pyatakova <nataniel87@yandex.ru>
2026-08-20 18:34:31 +0300

    Исправление ошибок стиля

--- src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -7,7 +7,7 @@ import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
-public class MpaRating implements Comparable<MpaRating>{
+public class MpaRating implements Comparable<MpaRating> {
 
     @NotNull
     private Integer id;
